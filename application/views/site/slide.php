89855e11011a50d5c02a0322dad1a173555b3044
VPhuongDuy <vd@embrace-it.com>
2020-09-11 16:34:12 +0700

    backup date 11-09

--- application/views/site/slide.php
+++ application/views/site/slide.php
@@ -12,7 +12,7 @@
       ?>
       <div class="col-lg-3 col-sm-6 mb-2 mb-lg-0 px-1 ">
         <article class="card bg-dark text-center text-white border-0 rounded-0">
-          <img class="card-img rounded-0 img-fluid w-100" style="min-height: 476px;" src="<?php echo $data->image_link != '' ? base_url('upload/stories/'.$data->image_link) :  base_url('upload/stories/default.jpg')?>" alt="<?php echo $data->description?>">
+          <img class="card-img rounded-0 img-fluid w-100" style="min-height: 476px;" src="<?php echo $data->image_link != '' ? base_url('upload/stories/'.$data->image_link) :  base_url('upload/stories/default.jpg')?>" alt="<?php echo $data->name?>">
           <div class="card-img-overlay">
             <div class="card-content">
               <p class="text-uppercase"><?php 
@@ -27,7 +27,7 @@
           </div>
         </article>
       </div>
-        <?php } endforeach ?>
+        <?php } endforeach; ?>
     </div>
   </div>
 </section>
