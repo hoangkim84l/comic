5f6b71db1f7f98b2b6541bd77348a14124c455dc
VPhuongDuy <vd@embrace-it.com>
2020-09-22 08:58:32 +0700

    change image footer

--- application/views/site/head.php
+++ application/views/site/head.php
@@ -60,3 +60,13 @@ $(function() {
 </script>
 <style>.raty img{width:16px !important;height:16px !important;}</style>
 <!--End raty --> 
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-178674513-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-178674513-1');
+</script>
