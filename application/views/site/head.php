3ef5c5ee48b030a332621e65dbdc8e275a808265
VPhuongDuy <vd@embrace-it.com>
2020-11-02 14:38:24 +0700

    add seo title

--- application/views/site/head.php
+++ application/views/site/head.php
@@ -26,14 +26,45 @@
 <meta name="author" content="<?php echo $support->author?>" />
 <meta name="copyright" content="<?php echo $support->copyright?>" />
 <!--Meta seo web-->
-<title><?php if(isset($stories) && ($stories->site_title != "")){ echo $stories->site_title;}elseif(isset($chapter) && ($chapter->site_title != "")){ echo $chapter->site_title;}else{echo $support->site_title;}?></title>
+<?php
+ $url = $this->uri->rsegment('1');
+ //contact //stories //user
+if($url == "contact"){
+  $seo_title_custom = "Liên hệ - Cafesuanovel";
+  $seo_desc_custom = "Liên hệ - Lướt cà phê sữa, muốn đọc nữa, hông muốn dừng - Đọc truyện online tại cafesuanovel.";
+}
+elseif($url == "stories"){
+  $seo_title_custom = "Truyện - Cafesuanovel";
+  $seo_desc_custom = "Đọc truyện online tại cafesuanovel với nhiều thể loại Sports, Trinh thám, Psychological, School Life, Harem, Horror, Comedy, Chuyển Sinh, Anime, Ngôn tình, Đam mỹ, Fantasy, Oneshot, Magic, Action, Manga/Comic, Romance, Shoujo, Adventure, Ecchi cập nhật mỗi ngày.";
+}
+elseif($url == "user"){
+  $subUrl = $this->uri->rsegment('2');
+  if($subUrl == "register"){
+    $seo_title_custom = "Đăng ký - Cafesuanovel";
+    $seo_desc_custom = "Đăng ký thành viên - Lướt cà phê sữa, muốn đọc nữa, hông muốn dừng - Đọc truyện online tại cafesuanovel.";
+  }elseif($subUrl == "login"){
+    $seo_title_custom = "Đăng nhập - Cafesuanovel";
+    $seo_desc_custom = "Đăng nhập - Lướt cà phê sữa, muốn đọc nữa, hông muốn dừng - Đọc truyện online tại cafesuanovel.";
+  }else{
+    $seo_title_custom = "Thành viên - Cafesuanovel";
+    $seo_desc_custom = "Thành viên - Lướt cà phê sữa, muốn đọc nữa, hông muốn dừng - Đọc truyện online tại cafesuanovel.";
+  }
+}
+else{
+  $seo_title_custom = $support->site_title;
+  $seo_desc_custom = $support->site_desc;
+}
+
+ var_dump($url);
+?>
+<title><?php if(isset($stories) && ($stories->site_title != "")){ echo $stories->site_title;}elseif(isset($chapter) && ($chapter->site_title != "")){ echo $chapter->site_title;}else{echo $seo_title_custom;}?></title>
 <link rel="canonical" href="<?php echo current_url();?>" />
 <meta name="keywords" content="<?php if(isset($stories) && ($stories->meta_key != "")){ echo $stories->meta_key;}elseif(isset($chapter) && ($chapter->meta_key != "")){ echo $chapter->meta_key;}else{echo $support->site_key;}?>" />
-<meta name="description" content="<?php if(isset($stories) && ($stories->meta_desc != "")){ echo $stories->meta_desc;}elseif(isset($chapter) && ($chapter->meta_desc != "")){ echo $chapter->meta_desc;}else{echo $support->site_desc;}?>" />
+<meta name="description" content="<?php if(isset($stories) && ($stories->meta_desc != "")){ echo $stories->meta_desc;}elseif(isset($chapter) && ($chapter->meta_desc != "")){ echo $chapter->meta_desc;}else{echo $seo_desc_custom;}?>" />
 <!--Meta seo web-->
 
 <!--Meta Geo-->
-<meta name="DC.title" content="<?php if(isset($stories) && ($stories->site_title != "")){ echo $stories->site_title;}elseif(isset($chapter) && ($chapter->site_title != "")){ echo $chapter->site_title;}else{echo $support->site_title;}?>" />
+<meta name="DC.title" content="<?php if(isset($stories) && ($stories->site_title != "")){ echo $stories->site_title;}elseif(isset($chapter) && ($chapter->site_title != "")){ echo $chapter->site_title;}else{echo $seo_title_custom;}?>" />
 <meta name="geo.region" content="<?php echo $support->geo_region?>" />
 <meta name="geo.placename" content="<?php echo $support->geo_placename?>" />
 <meta name="DC.identifier" content="<?php echo current_url();?>" />
@@ -43,7 +74,7 @@
 <meta property="og:title" typeof="WebPage" content="<?php if(isset($stories) && ($stories->meta_key != "")){ echo $stories->meta_key;}elseif(isset($chapter) && ($chapter->meta_key != "")){ echo $chapter->meta_key;}else{echo $support->site_key;}?>" />
 <meta property="og:url" typeof="WebPage" content="<?php echo current_url();?>" />
 <meta property="og:site_name" typeof="WebPage" content="<?php echo current_url();?>" />
-<meta property="og:description" typeof="WebPage" content="<?php if(isset($stories) && ($stories->meta_desc != "")){ echo $stories->meta_desc;}elseif(isset($chapter) && ($chapter->meta_desc != "")){ echo $chapter->meta_desc;}else{echo $support->site_desc;}?>" />
+<meta property="og:description" typeof="WebPage" content="<?php if(isset($stories) && ($stories->meta_desc != "")){ echo $stories->meta_desc;}elseif(isset($chapter) && ($chapter->meta_desc != "")){ echo $chapter->meta_desc;}else{echo $seo_desc_custom;}?>" />
 <meta property="og:type" typeof="WebPage" content="<?php echo $support->og_type?>" />
 <!-- jQuery -->
 <script src="<?php echo public_url()?>site/plugins/jQuery/jquery.min.js"></script>
